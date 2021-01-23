7f4c9d0a8edbf4e6a3247c1b3589775c9676f362
guimcaballero <guim.caballero@gmail.com>
2021-01-23 12:21:31 +0100

    Fancy failing arrows

--- src/arrows.rs
+++ src/arrows.rs
@@ -113,6 +113,21 @@ fn spawn_arrows(
 fn move_arrows(time: Res<Time>, mut query: Query<(&mut Transform, &Arrow)>) {
     for (mut transform, arrow) in query.iter_mut() {
         transform.translation.x += time.delta_seconds() * arrow.speed.value();
+
+        let distance_after_target = transform.translation.x - (TARGET_POSITION + THRESHOLD);
+        if distance_after_target >= 0.02 {
+            // Move the arrow down if it's past the target
+            transform.translation.y -= time.delta_seconds() * distance_after_target * 2.;
+
+            // Change the scale according to how far away the arrow is
+            let scale = ((100. - distance_after_target / 3.) / 100.).max(0.2);
+            transform.scale = Vec3::splat(scale);
+
+            // Rotate the arrow according to distance and speed
+            transform.rotate(Quat::from_rotation_z(
+                -distance_after_target * arrow.speed.multiplier() / 460.,
+            ));
+        }
     }
 }
 
